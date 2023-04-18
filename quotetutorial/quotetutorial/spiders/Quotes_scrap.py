81e2fe1eb620fcb0346051829c7e80d57e3cd913
lakshmi-scrap <130909391+lakshmi-scrap@users.noreply.github.com>
2023-04-18 20:36:40 +0530

    Update Quotes_scrap.py

--- quotetutorial/quotetutorial/spiders/Quotes_scrap.py
+++ quotetutorial/quotetutorial/spiders/Quotes_scrap.py
@@ -1,23 +1,3 @@
-# import scrapy
-
-
-# class FirstScrpy(scrapy.Spider):
-#     name = 'quotes'
-    
-#     start_urls = ['https://quotes.toscrape.com/']
-    
-#     def parse(self,response):
-#         # title = response .css('title::text').extract()
-#         # yield {'titletext' : title }
-        
-#         all_div_quotes = response.css('div.quote')
-#         title = all_div_quotes.css('span.text::text').extract()
-#         author = all_div_quotes.css('.author::text').extract()
-#         tag = all_div_quotes.css('.tag::text').extract()
-        
-#         yield { 'title' : title, 'author' : author, 'tags': tag }
-        
-        
 import scrapy
 
 class FirstScrpy(scrapy.Spider):
